89832d24e0296ba1526f020f4bd31feed76027d2
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-04-25 18:46:38 +0100

    Modify the way packages are autoinstalled
    Hopefully, it now works.

--- emacs/init.el
+++ emacs/init.el
@@ -8,44 +8,29 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;; List of packages to be installed!
+(setq package-list '(
+		     ;; Essential
+		     'evil
+		     ;; Eyecandy
+		     'darktooth-theme
+		     'solarized-theme))
 
-(setq package-enable-at-startup nil)
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+			 ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")
+			 ("melpa" . "http://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
-(defun ensure-package-installed (&rest packages)
-"Assure every package is installed, ask for installation if it’s not.
-Return a list of installed packages or nil for every skipped package."
-(mapcar
-(lambda (package)
-    (if (package-installed-p package)
-	nil
-    (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-
-	package)))
-packages))
-
-;; Make sure to have downloaded archive description.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-;; Activate installed packages
 (package-initialize)
 
-;;; Installing packages
-    ;; List of packages you wish to be installed:
-(ensure-package-installed
- ;; Essential
- 'evil
- ;; Eyecandy
- 'darktooth-theme
- 'solarized-theme)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;;; =============== Essential ===============
 
