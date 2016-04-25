2d797739e3156f5d8bb57a2d186a4676579f75ca
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-04-25 21:59:33 +0100

    Disable the mouse

--- emacs/init.el
+++ emacs/init.el
@@ -115,6 +115,15 @@
 
 ;;; =============== Tweaks ===============
 
+;; Disable the mouse!
+(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+  (global-unset-key k))
+(mouse-wheel-mode -1)
+
 ;; Scrolling
 (when (boundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
