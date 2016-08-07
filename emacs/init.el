16c474cbc720fc0d604f60cc481ec491d7e78513
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-08-07 18:51:51 +0100

    Add evil-exchange and fix font problem
    The previous way the font was set stopped the emacs daemon from
    starting.

--- emacs/init.el
+++ emacs/init.el
@@ -16,6 +16,7 @@
 		     evil
 		     evil-leader
 		     evil-mc
+		     evil-exchange
 		     ;; Languages
 		     haskell-mode
 		     lua-mode
@@ -23,6 +24,8 @@
 		     relative-line-numbers
 		     vertigo
 		     evil-snipe
+		     ;; Networking
+		     ;;jabber
 		     ;; Eyecandy
 		     darktooth-theme
 		     solarized-theme
@@ -45,7 +48,7 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;;; =============== Essential ===============
+; =============== Essential ===============
 
 ;;  --------------- Helm ---------------
 
@@ -95,6 +98,10 @@
 ;; ---------- Evil-MC ----------
 (require 'evil-mc)
 (global-evil-mc-mode 1)
+
+;; ---------- Evil-Exchange ----------
+(require 'evil-exchange)
+(evil-exchange-install)
 
 ;; =============== Languages ===============
 
@@ -161,15 +168,27 @@
 (setq evil-snipe-repeat-scope 'visible)
 (setq evil-snipe-spillover-scope 'visible)
 
-;;; =============== Eyecandy ===============
-
-;; --------------- Fonts ---------------
-(set-face-attribute 'default nil :font "Inconsolata-13")
-(set-frame-font "Inconsolata-13" nil t)
+;;;;; =============== Eyecandy ===============
+;;
+;;;; --------------- Fonts ---------------
+;;(set-face-attribute 'default nil :font "Inconsolata-13")
+;;(set-frame-font "Inconsolata-13" nil t)
+(add-to-list 'default-frame-alist '(font . "Inconsolata-13"))
 ;; --------------- Themes ---------------
 ;; Darktooth
-(require 'darktooth-theme)
-(load-theme 'darktooth t)
+;;(defun daio/load-theme ()
+;;  (interactive)
+;;  (require 'darktooth-theme)
+;;  (load-theme 'darktooth t))
+;;
+;;(add-hook 'after-init-hook #'daio/load-theme)
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+        (lambda (frame)
+            (select-frame frame)
+            (load-theme 'darktooth t)))
+    (load-theme 'darktooth t))
 
 ;; Solarized
 ;(require 'solarized-theme)
@@ -178,6 +197,27 @@
 
 ;; --------------- Evil-Org ---------------
 (require 'evil-org)
+
+;;; =============== Networking
+
+;; (require 'epa-file)
+;; (epa-file-enable)
+
+;; ---------- Jabber ----------
+;; ;; Define a list of my jabber.el accounts
+;; (setq jabber-account-list '(
+;;                             ("account@chat.facebook.com"
+;;                              (:network-server . "chat.facebook.com")
+;;                              (:connection-type . starttls)
+;;                              (:port . 5222)
+;;                              (:password . nil))))
+
+;; ;; Disable jabber.el presence notifications
+;; (remove-hook 'jabber-alert-presence-hooks
+;;              'sr-jabber-alert-presence-func)
+
+;; ;; Connect to all my jabber.el accounts on startup
+;; (jabber-connect-all)
 
 ;;; =============== Tweaks ===============
 ;; NO MORE INCESSANT BEEPING! ARGH!
@@ -195,15 +235,18 @@
   (call-process "mpg123" nil 0 nil
 		"/home/daniel/.emacs.d/serenade.mp3"))
 
+
+(require 'darktooth-theme)
+(load-theme 'darktooth t)
 ;; Disable the mouse!
-(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
-             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
-             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
-             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
-             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
-  (global-unset-key k))
-(mouse-wheel-mode -1)
-(mouse-avoidance-mode 'banish)
+;; (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+;;              [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+;;              [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+;;              [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+;;              [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+;;   (global-unset-key k))
+;; (mouse-wheel-mode -1)
+;; (mouse-avoidance-mode 'banish)
 
 ;; Scrolling
 (when (boundp 'scroll-bar-mode)
@@ -217,7 +260,5 @@
 ;; Default frame size
 (when window-system (set-frame-width (selected-frame) 82))
 
-
 (message "init.el has loaded successfully!")
 ;; init.el ends here
-
